352b43e654afec923f3bebbea3f6e49e42118b3d
AravindNico <aravindan.embedded@outlook.com>
2016-05-03 12:21:40 +0530

    update js

--- client/js/index.js
+++ client/js/index.js
@@ -3,8 +3,6 @@ var pub_key = "pub-c-913ab39c-d613-44b3-8622-2e56b8f5ea6d";
 var sub_key = "sub-c-8ad89b4e-a95e-11e5-a65d-02ee2ddab7fe";
 
 //Constants
-// var MAX_HISTORY_FETCH_LIMIT = 30; //In Minutes
-// var MAX_HISTORY_SAMPLE_LIMIT = 60; //Number of samples
 var BUTTON_TXT_TREND = 'Trend';
 var BUTTON_TXT_COUNTER = 'Counter';
 
@@ -23,13 +21,6 @@ var trend_graph = {
     'INR'   : null
 };
 
-var prevChange = {
-    'EUR'   : 0,
-    'AUD'   : 0,
-    'CNY'   : 0,
-    'INR'   : 0
-};
-
 var changeValue;
 
 //YOUR PUBNUB KEYS - Replace the publish_key and subscribe_key below with your own keys
@@ -39,50 +30,33 @@ var pubnub = PUBNUB.init({
 });
 
 //OnCLick trigger for Trend/Counter Button
-var sendRequest = function(p_name){
+var sendRequest = function(p_name,p_status){
     pubnub.publish({
-        channel : 'trendRequestChannel',
-        message : '{\"name\":\"' + p_name + '\"}'
+        channel : 'appRequestChannel',
+        message : '{\"name\":\"' + p_name + '\",\"requestType\":'+ p_status +'}'
     });
 }
 
-var valueChange = function(p_name,p_changeValue){
-    var l_currency_present_differance;
-    if(parseFloat(p_changeValue) > parseFloat(prevChange[p_name])){
-        var changes = p_changeValue - prevChange[p_name];
-        changes = changes.toFixed(2);
-        prevChange[p_name] = p_changeValue;
-        l_currency_present_differance = "+" + changes.toString(); 
-        displayDeltaDirectionArrow(p_name,l_currency_present_differance);
-    }
-    else if(parseFloat(p_changeValue) < parseFloat(prevChange[p_name])){
-        var changes = prevChange[p_name] - p_changeValue;
-        changes = changes.toFixed(2);
-        prevChange[p_name] = p_changeValue;
-        l_currency_present_differance = "-" + changes.toString();
-        displayDeltaDirectionArrow(p_name,l_currency_present_differance);
-    }
-    else{
-        displayDeltaDirectionArrow(p_name,"+0");
-        prevChange[p_name] = p_changeValue;
-    }
+var valueChange = function(p_name,p_direction,p_magnitude){
+        p_delta = p_direction + p_magnitude
+        displayDeltaDirectionArrow(p_name,p_delta);
 }
 
 var updatePrice = function(p_rcvmessage){
     var p_message = JSON.parse(p_rcvmessage);
-    if(p_message.requestType == 0){
+    if(p_message.responseType == 0){
         if(counterDisplayState[p_message.name] == true){
+
             $('#' + p_message['name']).html(p_message['value'])
             changeValue = p_message['value']
-            valueChange(p_message['name'],changeValue)
+            valueChange(p_message['name'],p_message['direction'],p_message['magnitude'])
             var date = new Date(p_message['time'] * 1000);
             var timeString = date.getUTCHours().toString() + ":" + date.getUTCMinutes().toString() + ":" + date.getUTCSeconds().toString();
             $('#' + p_message['name'] + '-time' ).html(timeString);
         }
         else{
             if(trend_graph[p_message.name] != null){
-                prevChange[p_message.name] = trend_graph[p_message.name][trend_graph[p_message.name].length-1]
-                valueChange(p_message.name,p_message.value)
+                valueChange(p_message.name,p_message['direction'],p_message['magnitude'])
                 trend_graph[p_message.name].shift();
                 trend_graph[p_message.name].push(p_message.value);
                 $('#'+p_message.name).sparkline(trend_graph[p_message.name])
@@ -91,21 +65,17 @@ var updatePrice = function(p_rcvmessage){
                 $('#' + p_message['name'] + '-time' ).html(timeString);
                 $('[data-index='+p_message.name+']').text(BUTTON_TXT_COUNTER);
             }
-            //If there is a connection error with server, waits for the server to connect
-            else if($('[data-index='+p_message.name+']').text() == "Loading.."){
-                try{
-                    sendRequest(p_message.name)
-                }
-                catch(err){
-                    console.log(err)
-                }
-            }
         }
     }
-	else if(p_message.requestType == 1){
+	else if(p_message.responseType == 1){
         if(counterDisplayState[p_message.name] == false){
             trend_graph[p_message['name']] = p_message['value'];
             $('#'+p_message.name).sparkline(trend_graph[p_message.name])
+            $('[data-index='+p_message.name+']').text(BUTTON_TXT_COUNTER);
+            valueChange(p_message['name'],p_message['direction'],p_message['magnitude'])
+            var date = new Date(p_message['time'] * 1000);
+            var timeString = date.getUTCHours().toString() + ":" + date.getUTCMinutes().toString() + ":" + date.getUTCSeconds().toString();
+            $('#' + p_message['name'] + '-time' ).html(timeString);
             $('[data-index='+p_message.name+']').text(BUTTON_TXT_COUNTER);
         }
     }
@@ -137,20 +107,26 @@ var displayDeltaDirectionArrow = function(p_name,p_delta){
 
 $(document ).ready(function() {
 	pubnub.subscribe({
-	    channel: 'exchangedata',
+	    channel: 'exchangedata1',
 	    message: updatePrice
 	});
+
+    sendRequest("EUR",0);
+    sendRequest("AUD",0);
+    sendRequest("CNY",0);
+    sendRequest("INR",0);
 
 	$('button').click(function(){
         if($(this).text() == BUTTON_TXT_TREND){
             //If the Button text is 'Trend' , send request to fetch historical values 
             $(this).text('Loading..');
-            sendRequest($(this).data('index'));
+            sendRequest($(this).data('index'),1);
             counterDisplayState[$(this).data('index')] = false;
         } else if($(this).text() == BUTTON_TXT_COUNTER) {
             //Change the text
             counterDisplayState[$(this).data('index')] = true;
             $(this).text(BUTTON_TXT_TREND);
+            sendRequest($(this).data('index'),0);
         }
     });
 });
